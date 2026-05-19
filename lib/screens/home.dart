795c33535de4e6c6f5e6d2c87e9f6db5c814ca19
paolapavan9-hub <paola.pavan.9@studenti.unipd.it>
2026-05-19 15:53:23 +0200

    paola 15.52

---
+++ lib/screens/home.dart
@@ -0,0 +1,10 @@
+// PAOLA ultima modifica 19/05/2026
+// Schermata Home: Grafico HRV oppure stress, 
+//                 bottone per impostazioni(pagina impostazioni)
+//                 bottone inizia serata (pagina serata)
+//                 non bevi da tot giorni, bravo!!
+import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+import 'package:hrv_app/screens/serata.dart';
+import 'package:hrv_app/screens/impostazioni.dart';
+//Prova 15.50
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,1 +1,6 @@
+<<<<<<< HEAD
+//Lo vedi?
+
+=======
 //ciao
+>>>>>>> c6db428dcd0e24f0d21e890eee4ebcb420707428
